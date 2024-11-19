34aac45008eaf90c1b88ff1af6f1241eb692315e
ReaperMaga <reapermaga@gmail.com>
2024-11-19 19:01:52 +0100

    Trying to fix multiple module dependency push

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,33 @@ plugins {
 group = "dev.reapermaga"
 version = "0.1.0"
 
+allprojects {
+    repositories {
+        mavenCentral()
+        mavenLocal()
+    }
+}
+
 subprojects {
     group = "dev.reapermaga.library"
+    apply(plugin = "maven-publish")
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+
+    java {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = project.group.toString()
+                artifactId = project.name
+                version = project.version.toString()
+                from(components["java"])
+            }
+        }
+    }
 }
 
 
