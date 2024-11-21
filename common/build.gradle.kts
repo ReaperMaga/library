650d05c1e5502321e8290a938c2066c1fdb51be2
ReaperMaga <reapermaga@gmail.com>
2024-11-21 20:34:36 +0100

    Added cached repository and changed ver of common lib

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "2.0.21"
 }
 
-version = "0.1.0"
+version = "0.1.1"
 
 dependencies {
     implementation("at.favre.lib:bcrypt:0.10.2")
--- common/src/main/kotlin/com/github/reapermaga/library/common/repository/LocalRepository.kt
+++ common/src/main/kotlin/com/github/reapermaga/library/common/repository/LocalRepository.kt
@@ -1,12 +1,8 @@
 package com.github.reapermaga.library.common.repository
 
-import java.lang.reflect.Field
-
 open class LocalRepository<T, ID>:Repository<T, ID> {
 
     val entities = mutableMapOf<ID, T>()
-
-    private var idClass : Field? = null
 
     override fun persist(entity : T) {
         val id = retrieveId(entity)
@@ -43,21 +39,9 @@ open class LocalRepository<T, ID>:Repository<T, ID> {
         return entities.size.toLong()
     }
 
-    private fun retrieveId(entity : T) : ID? {
-        if(entity != null) {
-            if(idClass == null) {
-                idClass = entity::class.java.declaredFields.first {
-                    it.isAccessible = true
-                    it.isAnnotationPresent(LocalId::class.java)
-                }
-            }
-            idClass?.let { return it.get(entity) as ID }
 
-        }
-        return null
-    }
 }
 
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.FIELD)
-annotation class LocalId
+annotation class Id
