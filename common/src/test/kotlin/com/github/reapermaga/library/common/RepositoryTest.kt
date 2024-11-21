8016bb3f6ca08a0ca8424cf16f813a1d1044e3dc
ReaperMaga <reapermaga@gmail.com>
2024-11-21 20:44:14 +0100

    feat: Added cached repository (#5)

--- common/src/test/kotlin/com/github/reapermaga/library/common/RepositoryTest.kt
+++ common/src/test/kotlin/com/github/reapermaga/library/common/RepositoryTest.kt
@@ -1,6 +1,7 @@
 package com.github.reapermaga.library.common
 
-import com.github.reapermaga.library.common.repository.LocalId
+import com.github.reapermaga.library.common.repository.CachedRepository
+import com.github.reapermaga.library.common.repository.Id
 import com.github.reapermaga.library.common.repository.LocalRepository
 import com.github.reapermaga.library.common.repository.Repository
 import org.junit.jupiter.api.Test
@@ -42,10 +43,47 @@ class RepositoryTest {
         assert(userRepository.count() == 0L)
     }
 
+    @Test
+    fun `Test cached repository`() {
+        val userRepository = CachedUserRepository()
+        val user = User("1", "1@1")
+
+        // Test persist and findById
+        userRepository.persist(user)
+        assert(userRepository.findById("1") == user)
+
+        // Test save
+        val updatedUser = user.copy(email = "new@1")
+        userRepository.save(updatedUser)
+        assert(userRepository.findById("1") == updatedUser)
+
+        // Test findAll
+        val allUsers = userRepository.findAll()
+        assert(allUsers.size == 1 && allUsers.contains(updatedUser))
+
+        // Test existsById
+        assert(userRepository.existsById("1"))
+
+        // Test count
+        assert(userRepository.count() == 1L)
+
+        // Test deleteById
+        userRepository.deleteById("1")
+        assert(userRepository.findById("1") == null)
+        assert(userRepository.count() == 0L)
+
+        // Test deleteAll
+        userRepository.persist(user)
+        userRepository.deleteAll()
+        assert(userRepository.count() == 0L)
+    }
+
 }
 
 interface UserRepository : Repository<User, String>
 
+class CachedUserRepository : CachedRepository<User, String>(LocalUserRepository())
+
 class LocalUserRepository : LocalRepository<User, String>(), UserRepository
 
-data class User(@LocalId val id: String, val email: String)
+data class User(@Id val id: String, val email: String)
