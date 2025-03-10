ce7d9dfc7a791404447ed06b650aca094ac6394c
ReaperMaga <reapermaga@gmail.com>
2025-03-10 06:00:17 +0100

    chore: Update Exposed version to 0.1.3 and enhance Maven publication dependencies

--- database/build.gradle.kts
+++ database/build.gradle.kts
@@ -2,13 +2,13 @@ plugins {
     kotlin("jvm")
 }
 
-class SourceSetEntry(val name: String, val version: String) {
-    val artifact: String
+class SourceSetEntry(val name : String, val version : String) {
+    val artifact : String
         get() = "database-${name}"
 }
 
 val sourceSetEntries = listOf<SourceSetEntry>(
-    SourceSetEntry("exposed", "0.1.2"),
+    SourceSetEntry("exposed", "0.1.3"),
 )
 
 version = "0.1.0"
@@ -21,7 +21,8 @@ sourceSets {
 
 sourceSetEntries.forEach {
     tasks.register<Jar>("${it.name}Jar") {
-        from(sourceSets[it.name].output)
+        val sourceSet = sourceSets[it.name]
+        from(sourceSet.output)
         archiveBaseName.set(it.artifact)
         archiveVersion.set(it.version)
     }
@@ -31,9 +32,37 @@ configure<PublishingExtension> {
     publications {
         sourceSetEntries.forEach {
             create<MavenPublication>(it.name) {
+                val task = tasks.named("${it.name}Jar")
                 artifact(tasks.named("${it.name}Jar"))
                 artifactId = it.artifact
                 version = it.version
+
+                val compileDeps = configurations["${it.name}CompileClasspath"]
+                val runtimeDeps = configurations["${it.name}RuntimeClasspath"]
+
+                pom {
+                    withXml {
+                        asNode().appendNode("dependencies").apply {
+                            runtimeDeps.resolvedConfiguration.firstLevelModuleDependencies.forEach {
+                                appendNode("dependency").apply {
+                                    appendNode("groupId", it.moduleGroup)
+                                    appendNode("artifactId", it.moduleName)
+                                    appendNode("version", it.moduleVersion)
+                                    appendNode("scope", "runtime")
+                                }
+                            }
+                            compileDeps.resolvedConfiguration.firstLevelModuleDependencies.forEach {
+                                appendNode("dependency").apply {
+                                    appendNode("groupId", it.moduleGroup)
+                                    appendNode("artifactId", it.moduleName)
+                                    appendNode("version", it.moduleVersion)
+                                    appendNode("scope", "compile")
+                                }
+                            }
+                        }
+
+                    }
+                }
             }
         }
     }
@@ -46,9 +75,9 @@ repositories {
 dependencies {
     // Exposed dependencies
     val exposedVersion = "0.59.0"
-    api("exposed", "org.jetbrains.exposed:exposed-core:$exposedVersion")
-    api("exposed", "org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
-    api("exposed", "org.jetbrains.exposed:exposed-java-time:$exposedVersion")
+    implementation("exposed", "org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("exposed", "org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("exposed", "org.jetbrains.exposed:exposed-java-time:$exposedVersion")
 
     implementation("exposed", project(":common"))
 
