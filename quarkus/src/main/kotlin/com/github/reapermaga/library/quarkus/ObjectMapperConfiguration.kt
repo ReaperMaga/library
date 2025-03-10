74573b0f174276aab86e91b6d2441d585f1fb996
ReaperMaga <reapermaga@gmail.com>
2025-03-11 00:56:46 +0100

    feat: Add ObjectMapperConfiguration to customize Jackson ObjectMapper

---
+++ quarkus/src/main/kotlin/com/github/reapermaga/library/quarkus/ObjectMapperConfiguration.kt
@@ -0,0 +1,22 @@
+package com.github.reapermaga.library.quarkus
+
+import com.fasterxml.jackson.databind.MapperFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.json.JsonMapper
+import io.quarkus.arc.All
+import io.quarkus.jackson.ObjectMapperCustomizer
+import jakarta.inject.Singleton
+
+class ObjectMapperConfiguration {
+
+    @Singleton
+    fun objectMapper(@All customizers : MutableList<ObjectMapperCustomizer>) : ObjectMapper {
+        val mapper = JsonMapper.builder()
+            .configure(MapperFeature.PROPAGATE_TRANSIENT_MARKER, true)
+            .build()
+        for (customizer in customizers) {
+            customizer.customize(mapper)
+        }
+        return mapper
+    }
+}
